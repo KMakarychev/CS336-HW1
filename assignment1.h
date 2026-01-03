e4a7b37b69fe75e9125fa297442fc1fdb5e0edb5
Konstantin Makarychev <43357379+KMakarychev@users.noreply.github.com>
2026-01-03 14:18:24 -0600

    Initial commit

---
+++ assignment1.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+std::string DefaultProblemSet();
+int FindKey(const std::vector<int> &encryptedName,
+            int n,
+            const std::string &restaurantList);
